a7d061d02ce437866de9d25e34b5f37ad0628c9e
Nick Frattaroli <Nafratt@gmail.com>
2019-09-10 13:32:23 -0400

    adding press start to begin

---
+++ assets/javascript/Javascript.js
@@ -0,0 +1,12 @@
+function startbutton(){
+    var x = document.getElementById("rules","startbtn");
+    if (x.style.display === "none") {
+      x.style.display = 100;
+    } else {
+      x.style.opacity = 0;
+    }
+  }
+  function timer() {
+    setTimeout(function(){ ; }, 30000);
+  }
+  
